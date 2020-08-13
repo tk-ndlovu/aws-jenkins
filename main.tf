8b2cad4b62e983b388842894d6a8fd671a1b392f
tk-ndlovu <64078782+tk-ndlovu@users.noreply.github.com>
2020-08-13 22:01:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-terraform {
-backend "remote"{
-hostname = "app.terraform.io" // this is the default URL for Terraform Cloud
-organization = "urburnxpert"
-workspaces {
-name = "aws-jenkins"
-}
-}
-}
+# terraform {
+# backend "remote"{
+# hostname = "app.terraform.io" // this is the default URL for Terraform Cloud
+# organization = "urburnxpert"
+# workspaces {
+# name = "aws-jenkins"
+# }
+# }
+# }
