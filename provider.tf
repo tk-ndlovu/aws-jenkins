9dad80431e8f85912cbf870ed81398444b2ca54a
tk-ndlovu <64078782+tk-ndlovu@users.noreply.github.com>
2020-08-13 23:06:54 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "eu-west-1"
-    AWS_ACCESS_KEY_ID = var.AWS_ACCESS_KEY_ID
-    AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
+    aws_access_key_id = var.AWS_ACCESS_KEY_ID
+    aws_secret_access_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 variable "AWS_ACCESS_KEY_ID" {
